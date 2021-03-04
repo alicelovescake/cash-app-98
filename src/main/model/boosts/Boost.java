c320fd77bcfadc4af95e652ee7d73db752cb4c52
Alice Zhao <azhao991@gmail.com>
2021-03-03 20:38:59 -0800

    Add code coverage for  BoostTest class

--- src/main/model/boosts/Boost.java
+++ src/main/model/boosts/Boost.java
@@ -4,6 +4,8 @@ import model.Transaction;
 
 public interface Boost {
     double cashBack = 0.01;
+
     boolean applyBoost(Transaction transaction);
+
     BoostType getBoostType();
 }
--- src/test/model/BoostTest.java
+++ src/test/model/BoostTest.java
@@ -1,19 +1,22 @@
 package model;
 
-import model.boosts.Boost;
-import model.boosts.FoodieBoost;
-import model.boosts.HighRollerBoost;
-import model.boosts.ShopaholicBoost;
+import model.boosts.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BoostTest {
 
     private Account testAccountA;
+    private Account testRetailAccount;
+    private Account testRestaurantAccount;
+    private Account testCafeAccount;
     private User testUserA;
 
-    private BusinessUser testBusinessUser;
-    private CreditCard testCard;
+    private BusinessUser testBusinessUserRetail;
+    private BusinessUser testBusinessUserRestaurant;
+    private BusinessUser testBusinessUserCafe;
     private  Boost highRoller;
     private  Boost shopaholic;
     private  Boost foodie;
@@ -21,10 +24,16 @@ public class BoostTest {
     @BeforeEach
     void setUp() {
         testUserA = new PersonalUser("$alicelovescake", "Vancouver", "Alice", "Zhao");
-        testCard = new CreditCard("Visa", 123456, 2025, 12);
-        testBusinessUser = new BusinessUser(
+        testBusinessUserRetail = new BusinessUser(
                 "$amazon", "Seattle", "Amazon", BusinessUser.BusinessType.RETAILER);
-        testAccountA = new Account(testUserA, 100.50);
+        testBusinessUserRestaurant = new BusinessUser(
+                "$indian", "Seattle", "Indian Food", BusinessUser.BusinessType.RESTAURANT);
+        testBusinessUserCafe = new BusinessUser(
+                "$starbucks", "Seattle", "Starbucks", BusinessUser.BusinessType.CAFE);
+        testAccountA = new Account(testUserA, 2000);
+        testRetailAccount = new Account(testBusinessUserRetail, 100);
+        testRestaurantAccount = new Account(testBusinessUserRestaurant, 100);
+        testCafeAccount = new Account(testBusinessUserCafe, 100);
         highRoller = new HighRollerBoost();
         shopaholic = new ShopaholicBoost();
         foodie = new FoodieBoost();
@@ -33,6 +42,48 @@ public class BoostTest {
 
     @Test
     void testConstructor() {
+        assertEquals(BoostType.HIGHROLLER, highRoller.getBoostType());
+        assertEquals(BoostType.SHOPAHOLIC, shopaholic.getBoostType());
+        assertEquals(BoostType.FOODIE, foodie.getBoostType());
+    }
 
+    @Test
+    void testApplyBoostHighRollerCashBack(){
+        testAccountA.addBoost(highRoller);
+        Transaction transaction =
+                new Transaction(testRetailAccount, testAccountA, 1000, Transaction.Type.EXCHANGE);
+        assertTrue(highRoller.applyBoost(transaction));
+        assertEquals(1100, testAccountA.getBalance());
+    }
+
+    @Test
+    void testApplyBoostShopaholicCashBack(){
+        testAccountA.addBoost(shopaholic);
+        Transaction transaction =
+                new Transaction(testRetailAccount, testAccountA, 100, Transaction.Type.EXCHANGE);
+        assertTrue(shopaholic.applyBoost(transaction));
+        assertEquals(1905, testAccountA.getBalance());
+    }
+
+    @Test
+    void testApplyBoostFoodieCashBack(){
+        testAccountA.addBoost(foodie);
+        Transaction transaction =
+                new Transaction(testCafeAccount, testAccountA, 10, Transaction.Type.EXCHANGE);
+        assertTrue(foodie.applyBoost(transaction));
+        assertEquals(1990.30, testAccountA.getBalance());
+
+        Transaction transaction2 =
+                new Transaction(testRestaurantAccount, testAccountA, 100, Transaction.Type.EXCHANGE);
+        assertTrue(foodie.applyBoost(transaction2));
+        assertEquals(1893.3, testAccountA.getBalance());
+    }
+
+    @Test
+    void testApplyBoostNoCashBack(){
+        Transaction transaction =
+                new Transaction(testRetailAccount, testAccountA, 999, Transaction.Type.EXCHANGE);
+        assertFalse(highRoller.applyBoost(transaction));
+        assertEquals(1001, testAccountA.getBalance());
     }
 }
